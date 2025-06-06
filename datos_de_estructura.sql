94814c4e19b87f14cf71629abfd0148d693f3a25
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-06 09:05:18 -0500

    fix: :sparkles: se le agregaron mas productos y mas cliente
    Ç

--- datos_de_estructura.sql
+++ datos_de_estructura.sql
@@ -1,10 +1,18 @@
 -- 1. **Registrar un nuevo cliente:**
-INSERT INTO clientes (nombre ,telefono ,direccion ) VALUES ('DARWIN', '3124060135', 'calle 45 #12-43' )
+INSERT INTO clientes (nombre ,telefono ,direccion ) 
+    VALUES ('DARWIN', '3124060135', 'calle 45 #12-43' ),
+    ('SANTIAGO', '3214745642', 'calle 98 #15-33' ),
+    ('RONAL',   '3184545423', 'calle 43 #12-63' )
+    ('NICOLAS', '3124846888', 'calle 455 #152-73' )
+    ('SARA',    '3125454148', 'calle 27 #127-03' )
+
 
 
 2. **Agregar un nuevo producto (pizza) al menú:**
 
-INSERT INTO productos ( nombre  ,
-tipo ,
-tamaño ,
-precio ) VALUES ('Pizza de la casa','pizza', 'mediana', 4500.00)
+INSERT INTO productos ( nombre ,tipo, tamaño, precio ) 
+VALUES  ('Pizza de la casa','pizza', 'mediana', 4500.00),
+        ('Pizza mixta','pizza', 'pequeña', 4200.00),
+        ('Pizza cuatro quesos','pizza', 'mediana', 4500.00),
+        ('Pizza hawaiana','pizza', 'mediana', 4500.00),
+        ('Pizza de pepperoni','pizza', 'mediana', 4500.00)
