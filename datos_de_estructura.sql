984c5979b47cf9c7b87297c660f1150adebd7199
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-06 09:18:54 -0500

    feat: :card_file_box: 3. **Registrar una bebida en el menú:**

--- datos_de_estructura.sql
+++ datos_de_estructura.sql
@@ -2,17 +2,28 @@
 INSERT INTO clientes (nombre ,telefono ,direccion ) 
     VALUES ('DARWIN', '3124060135', 'calle 45 #12-43' ),
     ('SANTIAGO', '3214745642', 'calle 98 #15-33' ),
-    ('RONAL',   '3184545423', 'calle 43 #12-63' )
-    ('NICOLAS', '3124846888', 'calle 455 #152-73' )
-    ('SARA',    '3125454148', 'calle 27 #127-03' )
+    ('RONAL',   '3184545423', 'calle 43 #12-63' ),
+    ('NICOLAS', '3124846888', 'calle 455 #152-73' ),
+    ('SARA',    '3125454148', 'calle 27 #127-03' );
 
 
 
-2. **Agregar un nuevo producto (pizza) al menú:**
+-- 2. **Agregar un nuevo producto (pizza) al menú:**
 
 INSERT INTO productos ( nombre ,tipo, tamaño, precio ) 
 VALUES  ('Pizza de la casa','pizza', 'mediana', 4500.00),
         ('Pizza mixta','pizza', 'pequeña', 4200.00),
         ('Pizza cuatro quesos','pizza', 'mediana', 4500.00),
         ('Pizza hawaiana','pizza', 'mediana', 4500.00),
-        ('Pizza de pepperoni','pizza', 'mediana', 4500.00)
+        ('Pizza de pepperoni','pizza', 'mediana', 4500.00);
+
+-- 3. **Registrar una bebida en el menú:**
+
+INSERT INTO productos ( nombre ,tipo, tamaño, precio ) 
+VALUES  ('PESSI','bebida', NULL, 5500.00),
+        ('FANTA','bebida', NULL, 2200.00),
+        ('COCA COLA','bebida', NULL, 4500.00),
+        ('CERVESA','bebida', NULL, 3000.00),
+        ('TE','bebida', NULL, 1500.00);
+
+
