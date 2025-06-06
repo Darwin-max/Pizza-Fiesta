df69064b8da956bb8ed2b85330626ab664602786
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-06 08:56:35 -0500

    feat: :card_file_box: 2. **Agregar un nuevo producto (pizza) al menú:**

--- datos_de_estructura.sql
+++ datos_de_estructura.sql
@@ -1,2 +1,10 @@
 -- 1. **Registrar un nuevo cliente:**
 INSERT INTO clientes (nombre ,telefono ,direccion ) VALUES ('DARWIN', '3124060135', 'calle 45 #12-43' )
+
+
+2. **Agregar un nuevo producto (pizza) al menú:**
+
+INSERT INTO productos ( nombre  ,
+tipo ,
+tamaño ,
+precio ) VALUES ('Pizza de la casa','pizza', 'mediana', 4500.00)
