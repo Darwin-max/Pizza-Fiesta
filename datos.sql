a4e1e14a8965935699f2f3d6f5d9c63ab469ec3e
Darwin-max <189870390+Darwin-max@users.noreply.github.com>
2025-06-06 08:29:58 -0500

    feat: :card_file_box: Se termina la estructura de base de datos de la pizza-fiesta

--- datos.sql
+++ datos.sql
@@ -22,3 +22,63 @@ precio_adicional DECIMAL(10,2) DEFAULT 0.00
 );
 
 
+CREATE TABLE combos (
+id_combo INT AUTO_INCREMENT PRIMARY KEY,
+nombre VARCHAR(100) NOT NULL,
+descripcion TEXT,
+precio DECIMAL(10,2) NOT NULL
+);
+
+
+CREATE TABLE combo_productos (
+id_combo INT,
+id_producto INT,
+PRIMARY KEY (id_combo, id_producto),
+FOREIGN KEY (id_combo) REFERENCES combos(id_combo),
+FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+);
+
+
+CREATE TABLE pedidos (
+id_pedido INT AUTO_INCREMENT PRIMARY KEY,
+id_cliente INT,
+hora_recogida TIME NOT NULL,
+fecha_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
+estado_pago ENUM('pendiente', 'pagado') DEFAULT 'pendiente',
+FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
+
+
+CREATE TABLE detalle_pedido (
+id_detalle INT AUTO_INCREMENT PRIMARY KEY,
+id_pedido INT,
+id_producto INT,
+cantidad INT DEFAULT 1,
+subtotal DECIMAL(10,2),
+FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido),
+FOREIGN KEY (id_producto) REFERENCES productos(id_producto)
+);
+
+--  Ingredientes por cada pizza personalizada en un pedido, Posibilidad de añadir ingredientes extra
+
+
+CREATE TABLE pizza_ingredientes (
+id_detalle INT,
+id_ingrediente INT,
+PRIMARY KEY (id_detalle, id_ingrediente),
+FOREIGN KEY (id_detalle) REFERENCES detalle_pedido(id_detalle),
+FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id_ingrediente)
+);
+
+
+CREATE TABLE pagos (
+id_pago INT AUTO_INCREMENT PRIMARY KEY,
+id_pedido INT,
+fecha_pago DATETIME DEFAULT CURRENT_TIMESTAMP,
+monto_total DECIMAL(10,2),
+metodo_pago ENUM('efectivo', 'tarjeta', 'transferencia'),
+FOREIGN KEY (id_pedido) REFERENCES pedidos(id_pedido)
+);
+
+
+
